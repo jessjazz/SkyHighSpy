401cffae2482b193694f0682fa9a82cfd71c0940
Jessica Marshall-Newson <54814522+jessjazz@users.noreply.github.com>
2021-04-22 16:25:35 +0100

    Update Meteor.cpp
    Updated constructor and removed create function.
    Replaced hard coded numbers with CONSTEXPR for screen size.

--- Meteor.cpp
+++ Meteor.cpp
@@ -1,10 +1,16 @@
 #include "MainGame.h"
 #include "Meteor.h"
 
+constexpr int DISPLAY_WIDTH = 1280;
+constexpr int DISPLAY_HEIGHT = 720;
+
 Meteor::Meteor(Point2f pos)
 	: GameObject(pos)
 {
 	m_pos = pos;
+	SetType(OBJ_METEOR);
+	SetDrawOrder(4);
+	SetUpdateOrder(3);
 }
 
 Meteor::~Meteor()
@@ -16,11 +22,11 @@ void Meteor::Update(GameState& gState)
 	// Wrap around to other side of the screen
 	if (m_pos.y < 0)
 	{
-		m_pos = { m_pos.x + 25, 720 };
+		m_pos = { m_pos.x + 25, DISPLAY_HEIGHT };
 	}
-	else if (m_pos.x > 1280)
+	else if (m_pos.x > DISPLAY_WIDTH)
 	{
-		m_pos = { 0, 720 };
+		m_pos = { 0, DISPLAY_HEIGHT };
 	}
 
 	m_pos += m_velocity;
@@ -31,14 +37,4 @@ void Meteor::Draw(GameState& gState) const
 	PlayBlitter& blit = PlayBlitter::Instance();
 
 	blit.DrawRotated(blit.GetSpriteId("meteor"), m_pos, 5.0f * gState.time, m_rot);
-}
-
-Meteor* Meteor::CreateMeteor(Point2f pos)
-{
-	Meteor* pMeteor{ nullptr };
-	pMeteor = new Meteor({ pos });
-	pMeteor->SetType(OBJ_METEOR);
-	pMeteor->SetDrawOrder(4);
-	pMeteor->SetUpdateOrder(3);
-	return pMeteor;
 }
